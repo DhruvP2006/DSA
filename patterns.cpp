56a3ebe0b9851bbaab0c25a5e52778fa34af92e6
DhruvP2006 <dhruv.pankhania@somaiya.edu>
2025-12-20 20:29:37 +0530

    patterns

--- patterns.cpp
+++ patterns.cpp
@@ -57,7 +57,7 @@ void pattern6(int n){
 
 void pattern7(int n){
     for(int k = 1; k<n; k++){
-    for(int i = 5-k; i>=0; i--){
+    for(int i = n-k; i>=0; i--){
       cout<<" ";
     }
     for(int j = 1; j<(2*k); j++){
@@ -129,34 +129,98 @@ for(int i = 1; i<n; i++){
 }
 
 void pattern13(int n){
+  int x = 0;
+  int k = 0;
   for(int i=1; i<=n; i++){
-    cout<<i;
-    for(int j = 1; j<i; j++){
-      cout<<"\n";
-    }
+    cout<<i<<" ";
+      if(k<x){
+        k++;
+        continue;
+      } else{
+        cout<<"\n";
+        x++;
+        k=0;
+      }
+    
   }
 }
 
-void pattern19 (int n){
-  for(int k = 0; k<n; k++){
-    for(int i = n-k; i>0; i--){
-      cout<<"*";
-    }
-    for(int j = 1; j<(2*k); j++){
-      cout<<" ";
-    }
+void pattern14(int n){
+  for(int i = 0; i<n; i++){
+    for(char j = 'A'; j<= 'A'+i; j++){
+      cout<<j;
+    } 
     cout<<"\n";
   }
-    for(int k = n; k>0; k--){
-    for(int i = 0; i<n-k; i++){
-      cout<<"*";
-    }
-    for(int j = 2*k-1; j>0; j--){
+}
+
+void pattern15(int n){
+  for(int i = n; i>0; i--){
+    for(char j = 'A'; j< 'A' + i; j++){
+      cout<<j;
+        }
+        cout<<"\n";
+  }
+
+}
+
+void pattern16(int n){
+char x = 'A';
+for(int i = 1; i<n; i++){
+  for(int j = 0; j<i; j++){
+    cout<<x;
+  }
+  cout<<"\n";
+  x = 'A' + i;
+}
+}
+
+void pattern17(int n){
+  for(int i = 0; i<n; i++){
+    char a = 'A';
+    for(int j = n-i; j>0; j--){
       cout<<" ";
+    }
+    for(int k = 0; k<2*i + 1; k++){
+      if(k< i){
+        cout<<a;
+        a++;
+      } else {
+        cout<<a;
+        a-= 1;
+      }
     }
     cout<<"\n";
   }
 }
+
+void pattern18(int n){
+  for(int i = 0; i<=n; i++){
+    for(char j = 'A'+n-i; j<'A'+n; j++){
+      cout<<j;
+    }
+    cout<<"\n";
+  }
+}
+
+void pattern19(int n){
+  for(int i = 0; i<=2*n; i++){
+    for(int j = 0; j< (2*n-i)/2; j++){
+      cout<<"*";
+    }
+    for(int j = 0; j<i; j++){
+      cout<<" ";
+    }
+    for(int j = 0; j<i; j++){
+      cout<<" ";
+    }
+    for(int j = 0; j< (2*n-i)/2; j++){
+      cout<<"*";
+    }
+    cout<<"\n";
+  }
+}
+
 
 
 int main(){
